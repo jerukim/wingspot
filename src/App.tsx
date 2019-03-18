9119948e93b2a7d83a83f1c002b7826f9affab1b
jeru <mikurej95@gmail.com>
2019-03-18 01:24:08 -0500

    added home/menu nav button placeholders

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ import * as React from 'react';
 import Routes from './routes';
 
 const App: React.FunctionComponent<{}> = () => {
-  return <Routes />;
+  return (
+    <div>
+      <h1>WINGSPOT</h1>
+      <Routes />
+      <h1>MENU</h1>
+    </div>
+  );
 };
 
 export default App;
