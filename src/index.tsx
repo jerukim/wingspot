3e666f8f38496cc2aef1129f66129ee538093f5f
jeru <mikurej95@gmail.com>
2019-03-17 22:25:49 -0500

    changed BrowserRouter to Router for history prop

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import App from './App';
 import history from './history';
 import './index.css';
