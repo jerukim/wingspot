f8947fb6ec086129dd5708fb986178934c688c6a
jeru <mikurej95@gmail.com>
2019-03-18 01:13:50 -0500

    added pathname to home component

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import Menu from './menu';
 
-const Home: React.FunctionComponent<{}> = () => {
+const Home: React.FunctionComponent<{ location: Location }> = ({
+  location,
+}) => {
+  const { pathname } = location;
   return (
     <div className="home">
       <h1>Home</h1>
+      <p>{pathname}</p>
       <Menu />
     </div>
   );
