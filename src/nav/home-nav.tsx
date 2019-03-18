acc99e7c6c437f7a0112140cbfd29f98a8e0b9ba
jeru <mikurej95@gmail.com>
2019-03-18 03:48:01 -0500

    changed HomeNav to be a react-router Link

--- src/nav/home-nav.tsx
+++ src/nav/home-nav.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
 import './home-nav.css';
+import { Link } from 'react-router-dom';
 
 const HomeNav: React.FunctionComponent<{}> = () => {
-  return <div className="home-nav">WINGSPOT</div>;
+  return (
+    <Link to="/" className="home-nav">
+      WINGSPOT
+    </Link>
+  );
 };
 
 export default HomeNav;
