59e71d9493b4b99b442544a60e12823092a33cc0
jeru <mikurej95@gmail.com>
2019-03-18 03:55:23 -0500

    made MenuNav into a react-router Link

--- src/nav/menu-nav.tsx
+++ src/nav/menu-nav.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
 import './menu-nav.css';
+import { Link } from 'react-router-dom';
 
 const MenuNav: React.FunctionComponent<{}> = () => {
-  return <div className="menu-nav">MENU</div>;
+  return (
+    <Link to="/menu" className="menu-nav">
+      MENU
+    </Link>
+  );
 };
 
 export default MenuNav;
