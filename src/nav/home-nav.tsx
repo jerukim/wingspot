a9c764de145f2efe2d2007c8b39d2bc763e2e148
jeru <mikurej95@gmail.com>
2019-03-18 01:25:01 -0500

    setup home-nav component

---
+++ src/nav/home-nav.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const HomeNav: React.FunctionComponent<{}> = () => {
+  return <div className="home-nav">WINGSPOT</div>;
+};
+
+export default HomeNav;
---
+++ src/nav/index.tsx
@@ -0,0 +1,1 @@
+export { default as HomeNav } from './home-nav';
